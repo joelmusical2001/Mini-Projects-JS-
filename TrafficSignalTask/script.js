1002dadef8f892d93150af2ac89a41644caec51f
joelmusical2001 <joelmusical2001@gmail.com>
2024-03-12 19:10:26 +0530

    First Commit

---
+++ TrafficSignalTask/script.js
@@ -0,0 +1,44 @@
+function stp() {
+    let a = document.getElementById("btn-1")
+    let b = document.getElementById("red")
+    let c = document.getElementById("btn-2")
+    let d = document.getElementById("yellow")
+    let e = document.getElementById("btn-3")
+    let f = document.getElementById("green")
+    a.style.backgroundColor = "red"
+    b.style.backgroundColor = "red"
+    c.style.backgroundColor = "#1D1C3E"
+    d.style.backgroundColor = "#494D66"
+    e.style.backgroundColor = "#1D1C3E"
+    f.style.backgroundColor = "#494D66"
+}
+function ready() {
+    let a = document.getElementById("btn-1")
+    let b = document.getElementById("red")
+    let c = document.getElementById("btn-2")
+    let d = document.getElementById("yellow")
+    let e = document.getElementById("btn-3")
+    let f = document.getElementById("green")
+    c.style.backgroundColor = "#F6CA4F"
+    d.style.backgroundColor = "#F6CA4F"
+    a.style.backgroundColor = "#1D1C3E"
+    b.style.backgroundColor = "#494D66"
+    e.style.backgroundColor = "#1D1C3E"
+    f.style.backgroundColor = "#494D66"
+
+}
+function go() {
+    let a = document.getElementById("btn-1")
+    let b = document.getElementById("red")
+    let c = document.getElementById("btn-2")
+    let d = document.getElementById("yellow")
+    let e = document.getElementById("btn-3")
+    let f = document.getElementById("green")
+    c.style.backgroundColor = "#1D1C3E"
+    d.style.backgroundColor = "#494D66"
+    a.style.backgroundColor = "#1D1C3E"
+    b.style.backgroundColor = "#494D66"
+    e.style.backgroundColor = "green"
+    f.style.backgroundColor = "green"
+
+}
